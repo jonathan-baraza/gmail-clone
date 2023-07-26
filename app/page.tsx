37cd4fb6c75feea7a501fdb33ee4dc3d44bce4d5
jonathan_baraza <jbaraza46@gmail.com>
2023-07-26 16:21:44 +0300

    welcome page init

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { FaUser } from "react-icons/fa";
 export const metadata: Metadata = {
   title: "Gmail",
   description: "Gmail clone",
@@ -7,5 +8,18 @@ export const metadata: Metadata = {
   },
 };
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="min-h-[100vh]  flex  items-center justify-center flex-col">
+      <div className="bg-[#ffab40] rounded-full p-8 mb-10">
+        <FaUser size={40} color="#402400" />
+      </div>
+      <div className="text-[#202124] text-[35px] font-semibold ">
+        Set up your new chrome profile
+      </div>
+      <div className="text-gray-500 text-lg">
+        To access your Chrome stuff across all your devices, sign in, then turn
+        on sync.
+      </div>
+    </div>
+  );
 }
