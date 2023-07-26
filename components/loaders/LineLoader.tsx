7b44a350c1fe179b5a126a81acd5347b098f0272
jonathan_baraza <jbaraza46@gmail.com>
2023-07-26 23:04:55 +0300

    line loader component

--- components/loaders/LineLoader.tsx
+++ components/loaders/LineLoader.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const LineLoader = () => {
   return (
-    <div>LineLoader</div>
-  )
-}
+    <div className="absolute overflow-x-hidden top-0 left-0 right-0 bg-gray-100 h-[4px] z-50">
+      <div id="lineLoader" className="w-1/2 absolute bg-[#1a73e8] h-full"></div>
+    </div>
+  );
+};
 
-export default LineLoader
+export default LineLoader;
