f31d17afdb61e3e230445a23ba2d104d3b2b1496
jonathan_baraza <jbaraza46@gmail.com>
2023-07-26 15:58:15 +0300

    app icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Gmail",
+  description: "Gmail clone",
+  icons: {
+    icon: "/icons/icon4.png",
+  },
+};
 export default function Home() {
   return <main>Google clone init</main>;
 }
