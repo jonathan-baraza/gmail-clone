df13b1ac06761b3219e1ebae083b0949d95e2975
jonathan_baraza <jbaraza46@gmail.com>
2023-07-26 21:45:59 +0300

    components

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
 import type { Metadata } from "next";
 import { FaUser } from "react-icons/fa";
+import { useRouter } from "next/navigation";
+import LineLoader from "@/components/loaders/LineLoader";
 export const metadata: Metadata = {
   title: "Gmail",
   description: "Gmail clone",
@@ -8,26 +11,36 @@ export const metadata: Metadata = {
   },
 };
 export default function Home() {
+  const router = useRouter();
   return (
-    <div className="min-h-[100vh]  flex  items-center justify-center flex-col">
-      <div className="bg-[#ffab40] rounded-full p-8 mb-10">
-        <FaUser size={40} color="#402400" />
+    <>
+      <LineLoader />
+      <div className="min-h-[100vh]  flex  items-center justify-center flex-col">
+        <div className="bg-[#ffab40] rounded-full p-8 mb-10">
+          <FaUser size={40} color="#402400" />
+        </div>
+        <img src="./welcomepic1.png" className="absolute left-0" />
+        <img src="./welcomepic2.png" className="absolute right-0" />
+        <div className="text-[#202124] text-[35px] font-semibold ">
+          Set up your new chrome profile
+        </div>
+        <div className="text-gray-500 text-lg mb-8">
+          To access your Chrome stuff across all your devices, sign in, then
+          turn on sync.
+        </div>
+        <button
+          onClick={() => router.push("/auth/Login")}
+          className="bg-[#1a73e8] text-sm py-2 w-[200px] hover:bg-[#1565d0] rounded text-white mb-4"
+        >
+          Sign in
+        </button>
+        <button
+          onClick={() => router.push("/auth/Register")}
+          className="bg-[#1a73e8] text-sm py-2 w-[200px] hover:bg-[#1565d0] rounded text-white"
+        >
+          Create an account
+        </button>
       </div>
-      <img src="./welcomepic1.png" className="absolute left-0" />
-      <img src="./welcomepic2.png" className="absolute right-0" />
-      <div className="text-[#202124] text-[35px] font-semibold ">
-        Set up your new chrome profile
-      </div>
-      <div className="text-gray-500 text-lg mb-8">
-        To access your Chrome stuff across all your devices, sign in, then turn
-        on sync.
-      </div>
-      <button className="bg-[#1a73e8] text-sm py-2 w-[200px] hover:bg-[#1565d0] rounded text-white mb-4">
-        Sign in
-      </button>
-      <button className="bg-[#1a73e8] text-sm py-2 w-[200px] hover:bg-[#1565d0] rounded text-white">
-        Create an account
-      </button>
-    </div>
+    </>
   );
 }
---
+++ components/loaders/LineLoader.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LineLoader = () => {
+  return (
+    <div>LineLoader</div>
+  )
+}
+
+export default LineLoader
