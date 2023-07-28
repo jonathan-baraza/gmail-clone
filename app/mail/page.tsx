0682d55049c5f6d75820a18a64527d6a2409419f
jonathan_baraza <jbaraza46@gmail.com>
2023-07-28 22:27:25 +0300

    fix: file structure

--- app/mail/page.tsx
+++ app/mail/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function Mail() {
   return (
     <div className="w-full h-[100vh] flex items-center justify-center">
-      <h1 className="font-bold text-3xl">Welcome to you mail page</h1>
+      <h1 className="font-bold text-3xl">Welcome to you mail page </h1>
     </div>
   );
 }
