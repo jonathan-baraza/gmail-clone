7faa48bc0424279d68b91d2f17329ec9349ae308
jonathan_baraza <jbaraza46@gmail.com>
2023-07-28 15:02:56 +0300

    feat: line loader overlay

--- components/loaders/LineLoader.tsx
+++ components/loaders/LineLoader.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const LineLoader = () => {
+const LineLoader = ({ overlay }: { overlay?: boolean }) => {
   return (
-    <div className="absolute overflow-x-hidden top-0 left-0 right-0 bg-gray-100 h-[4px] z-50">
-      <div id="lineLoader" className="w-1/2 absolute bg-[#1a73e8] h-full"></div>
+    <div
+      className={`absolute ${
+        overlay && "bottom-0 bg-white bg-opacity-40 "
+      }  overflow-x-hidden top-0 left-0 right-0  z-50`}
+    >
+      <div className={`w-full relative overflow-x-hidden rounded-xl h-[4px]`}>
+        <div
+          id="lineLoader"
+          className="w-1/2 absolute rounded-xl bg-[#1a73e8] h-full"
+        ></div>
+      </div>
     </div>
   );
 };
