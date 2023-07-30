452b023604a06c789310814a001810860cc5190b
jonathan_baraza <jbaraza46@gmail.com>
2023-07-30 12:05:56 +0300

    auth instance

--- config/firebase.ts
+++ config/firebase.ts
@@ -12,16 +12,6 @@ const firebaseConfig = {
   measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA-r-qHu00j0_CTH9hwmWhtdKoNF6hnSO8",
-//   authDomain: "fir-25211.firebaseapp.com",
-//   projectId: "fir-25211",
-//   storageBucket: "fir-25211.appspot.com",
-//   messagingSenderId: "807764382290",
-//   appId: "1:807764382290:web:e44fa50c619bb6250d858b",
-//   measurementId: "G-3S6HT981H2",
-// };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
