eb158f9fdb4f54d530babc0491b0ae43ae83be19
jonathan_baraza <jbaraza46@gmail.com>
2023-08-10 21:48:49 +0300

    main page info

--- app/mail/page.tsx
+++ app/mail/page.tsx
@@ -2,19 +2,19 @@
 import LineLoader from "@/components/loaders/LineLoader";
 import { auth } from "@/config/firebase";
 import { signOut } from "firebase/auth";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAppSelector } from "@/hooks/reduxHooks";
 function Mail() {
-  const user = useAppSelector((state) => state.auth.user);
+  const { user, isAuthenticated } = useAppSelector((state) => state.auth);
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
+  const [pageLoading, setPageLoading] = useState<boolean>(false);
   const handleSignOut = async () => {
     setLoading(true);
     localStorage.clear();
     try {
       await signOut(auth);
-      console.clear()
       router.push("/auth/signin");
     } catch (error) {
       //TODO: handle signout error better
@@ -23,20 +23,31 @@ function Mail() {
       setLoading(false);
     }
   };
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      return router.push("/auth/signin");
+    }
+  }, []);
+
   return (
     <>
       {loading && <LineLoader overlay={true} />}
-      <div className="w-full h-[100vh] flex flex-col items-center justify-center">
-        <h1 className="font-bold text-3xl">
-          Welcome {user?.username} to you mail page{" "}
-        </h1>
-        <button
-          onClick={handleSignOut}
-          className="text-white bg-[#1a73e8] hover:bg-[#0d4b9c] hover:cursor-pointer rounded text-sm px-6 py-3 hover:shadow mt-6"
-        >
-          Sign out
-        </button>
-      </div>
+      {pageLoading ? (
+        <div>Page Loading {String(isAuthenticated)}</div>
+      ) : (
+        <div className="w-full h-[100vh] flex flex-col items-center justify-center">
+          <h1 className="font-bold text-3xl">
+            Welcome {user?.username} to you mail page
+          </h1>
+          <button
+            onClick={handleSignOut}
+            className="text-white bg-[#1a73e8] hover:bg-[#0d4b9c] hover:cursor-pointer rounded text-sm px-6 py-3 hover:shadow mt-6"
+          >
+            Sign out
+          </button>
+        </div>
+      )}
     </>
   );
 }
