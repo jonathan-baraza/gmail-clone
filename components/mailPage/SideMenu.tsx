2ab7bf407c58b521ddd262e8d4c82b9756cab00f
jonathan_baraza <jbaraza46@gmail.com>
2023-09-07 20:37:28 +0300

    side menu icons

--- components/mailPage/SideMenu.tsx
+++ components/mailPage/SideMenu.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { HiPencil, HiInbox, HiOutlineStar } from "react-icons/hi";
+import {
+  HiPencil,
+  HiInbox,
+  HiOutlineStar,
+  HiOutlineDocument,
+} from "react-icons/hi";
+import { WiTime4 } from "react-icons/wi";
+import { MdLabelImportantOutline } from "react-icons/md";
+import { VscSend } from "react-icons/vsc";
+import { GrDocument } from "react-icons/gr";
 
 const SideMenu = () => {
-
-
   return (
     <div className="w-[18%] h-full ">
       <div className=" pt-4">
@@ -31,6 +38,38 @@ const SideMenu = () => {
             <span className="flex items-center">
               <HiOutlineStar className="" size={20} />
               <span className="ml-3 text-[15px]">Starred</span>
+            </span>
+            <span className="text-xs font-semibold"></span>
+          </div>
+          {/* Snoozed */}
+          <div className="flex items-center hover:bg-[#eaebef] justify-between px-2 hover:cursor-pointer  rounded-3xl px-4 py-[6px] text-[#444746]">
+            <span className="flex items-center">
+              <WiTime4 className="" size={20} />
+              <span className="ml-3 text-[15px]">Snoozed</span>
+            </span>
+            <span className="text-xs font-semibold"></span>
+          </div>
+          {/* Important */}
+          <div className="flex items-center hover:bg-[#eaebef] justify-between px-2 hover:cursor-pointer  rounded-3xl px-4 py-[6px] text-[#444746]">
+            <span className="flex items-center">
+              <MdLabelImportantOutline className="" size={20} />
+              <span className="ml-3 text-[15px]">Important</span>
+            </span>
+            <span className="text-xs font-semibold"></span>
+          </div>
+          {/* Sent */}
+          <div className="flex items-center hover:bg-[#eaebef] justify-between px-2 hover:cursor-pointer  rounded-3xl px-4 py-[6px] text-[#444746]">
+            <span className="flex items-center">
+              <VscSend className="" size={20} />
+              <span className="ml-3 text-[15px]">Sent</span>
+            </span>
+            <span className="text-xs font-semibold"></span>
+          </div>
+          {/* Drafts */}
+          <div className="flex items-center hover:bg-[#eaebef] justify-between px-2 hover:cursor-pointer  rounded-3xl px-4 py-[6px] text-[#444746]">
+            <span className="flex items-center">
+              <HiOutlineDocument className="" size={20} />
+              <span className="ml-3 text-[15px]">Drafts</span>
             </span>
             <span className="text-xs font-semibold"></span>
           </div>
