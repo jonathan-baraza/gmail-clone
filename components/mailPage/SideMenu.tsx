43d677d875065a59102d2f405438873304157fb6
jonathan_baraza <jbaraza46@gmail.com>
2023-09-07 20:19:55 +0300

    compose btns

--- components/mailPage/SideMenu.tsx
+++ components/mailPage/SideMenu.tsx
@@ -1,19 +1,28 @@
 import React from "react";
-import { HiPencil } from "react-icons/hi";
+import { HiPencil, HiInbox } from "react-icons/hi";
 
 const SideMenu = () => {
+
+
   return (
     <div className="w-[19%] h-full ">
-      <div className="pl-5 pt-4">
-        <div className="flex items-center space-x-2">
+      <div className=" pt-4">
+        <div className="ml-5 flex items-center space-x-2">
           <img src={"/gmail.svg"} className="h-[35px]" />
           <span className=" text-gray-700 text-2xl">Gmail</span>
         </div>
 
-        <div className="mt-5">
-          <div className="bg-[#c2e7ff] flex items-center justify-around p-3 rounded-xl w-[60%]">
-            <HiPencil size={25} />
-            <span className="">Compose</span>
+        <div className=" pl-3 mt-6 ">
+          <div className="bg-[#c2e7ff] flex items-center justify-around p-[14px] rounded-2xl w-[55%] hover:cursor-pointer hover:shadow-lg">
+            <HiPencil size={23} />
+            <span className="text-[16px]">Compose</span>
+          </div>
+          <div className="flex items-center justify-between px-2 bg-[#d3e3fd] hover:cursor-pointer mt-4 rounded-2xl px-3 py-1">
+            <span className="flex items-center">
+              <HiInbox className="" size={20} />
+              <span className="ml-3 text-sm">Inbox</span>
+            </span>
+            <span className="text-sm">746</span>
           </div>
         </div>
       </div>
