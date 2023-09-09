4712f2ce9079bd72862ff1c713a52f7e7af69548
jonathan_baraza <jbaraza46@gmail.com>
2023-09-09 13:37:36 +0300

    input focus toggler

--- components/mailPage/NavArea.tsx
+++ components/mailPage/NavArea.tsx
@@ -1,15 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 
 const NavArea = () => {
+  const [inputActive, setInputActive] = useState<boolean>(false);
   return (
     <div className="w-full py-2">
       <div>
-        <div className="flex items-center px-[6px] bg-[#eaf1fb] w-[70%]  rounded-3xl">
+        <div className={`flex items-center px-[6px] bg-[#eaf1fb] w-[70%]  rounded-3xl`}>
           <div className="mr-1 hover:cursor-pointer hover:bg-[#dde3ec] p-2 rounded-full">
             <HiOutlineSearch color={"#5f6368"} size={20} />
           </div>
           <input
+            onFocus={() => setInputActive(true)}
+            onBlur={() => setInputActive(false)}
             className="placeholder:text-[#5e5e63] placeholder:font-[400] py-3  bg-transparent  outline-none "
             placeholder="Search in emails"
           />
