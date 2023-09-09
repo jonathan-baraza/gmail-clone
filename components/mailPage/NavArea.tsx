182f903ae79c604857df0558f16652827c254c83
jonathan_baraza <jbaraza46@gmail.com>
2023-09-09 13:29:08 +0300

    search nav and icon

--- components/mailPage/NavArea.tsx
+++ components/mailPage/NavArea.tsx
@@ -3,12 +3,14 @@ import { HiOutlineSearch } from "react-icons/hi";
 
 const NavArea = () => {
   return (
-    <div className="w-full">
+    <div className="w-full py-2">
       <div>
-        <div className="flex items-center bg-[#eaf1fb]">
-          <HiOutlineSearch color={"#5f6368"} size={30} />
+        <div className="flex items-center px-[6px] bg-[#eaf1fb] w-[70%]  rounded-3xl">
+          <div className="mr-1 hover:cursor-pointer hover:bg-[#dde3ec] p-2 rounded-full">
+            <HiOutlineSearch color={"#5f6368"} size={20} />
+          </div>
           <input
-            className="placeholder:text-[#5e5e63]"
+            className="placeholder:text-[#5e5e63] placeholder:font-[400] py-3  bg-transparent  outline-none "
             placeholder="Search in emails"
           />
         </div>
