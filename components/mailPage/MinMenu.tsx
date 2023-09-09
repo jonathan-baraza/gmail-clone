4e6a0ba39b742ac0c977bf3aa01e8a9bffc330d7
jonathan_baraza <jbaraza46@gmail.com>
2023-09-09 14:27:16 +0300

    padding quick fix

--- components/mailPage/MinMenu.tsx
+++ components/mailPage/MinMenu.tsx
@@ -7,12 +7,12 @@ import { PiUsersThreeLight, PiVideoCamera } from "react-icons/pi";
 const MinMenu = () => {
   return (
     <div className="h-full bg-[#eaf1fb] flex-col items-center w-[6%] px-1">
-      <div className="p-3 rounded-full hover:cursor-pointer mt-3 hover:bg-[#d8dfe8] flex items-center justify-center w-3/4 mx-auto">
+      <div className="p-3 rounded-full hover:cursor-pointer mt-2 hover:bg-[#d8dfe8] flex items-center justify-center w-3/4 mx-auto">
         <AiOutlineMenu size={22} />
       </div>
 
       {/* Mail option */}
-      <div className="flex flex-col items-center mt-6">
+      <div className="flex flex-col items-center mt-5">
         <div className="p-2 relative  rounded-3xl hover:cursor-pointer mt-3 bg-[#d3e3fd] flex items-center justify-center w-[70%] mx-auto">
           <FaEnvelope size={18} color={"#041e49"} />
           <span className="bg-[#b3261e] text-white px-[3px] py-[2px] rounded-3xl text-[10px] font-bold absolute -top-[14%] border border-white border-2 right-0">
@@ -45,6 +45,7 @@ const MinMenu = () => {
         </div>
         <span className="text-xs font-semibold text-[#444746]">Meet</span>
       </div>
+      
     </div>
   );
 };
