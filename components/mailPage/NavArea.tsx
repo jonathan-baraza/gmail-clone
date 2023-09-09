dd09f27c9540caa93c2b3a4895a788c7bf995672
jonathan_baraza <jbaraza46@gmail.com>
2023-09-09 13:15:09 +0300

    nav input area dev

--- components/mailPage/NavArea.tsx
+++ components/mailPage/NavArea.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { HiOutlineSearch } from "react-icons/hi";
 
 const NavArea = () => {
-  return <div className="bg-red-200 w-full">NavArea</div>;
+  return (
+    <div className="w-full">
+      <div>
+        <div className="flex items-center bg-[#eaf1fb]">
+          <HiOutlineSearch color={"#5f6368"} size={30} />
+          <input
+            className="placeholder:text-[#5e5e63]"
+            placeholder="Search in emails"
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NavArea;
