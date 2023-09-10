4026ac44bc27be964f70655c7a292f5431cab19b
jonathan_baraza <jbaraza46@gmail.com>
2023-09-10 11:41:58 +0300

    mails head design

--- components/mailPage/MyMails.tsx
+++ components/mailPage/MyMails.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import MailsHead from "./mails/MailsHead";
 
 const MyMails = () => {
   return (
     <div className="w-full pr-4">
-      <div className="h-full w-full bg-white rounded-t-2xl p-3">MyMails</div>
+      <div className="h-full w-full bg-white rounded-t-2xl p-3">
+        <MailsHead/>
+      </div>
     </div>
   );
 };
