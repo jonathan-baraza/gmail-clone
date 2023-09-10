6a2bf4e604e6c3c133be9327350367ea09589a7b
jonathan_baraza <jbaraza46@gmail.com>
2023-09-10 11:50:31 +0300

    container position

--- components/mailPage/MyMails.tsx
+++ components/mailPage/MyMails.tsx
@@ -3,9 +3,9 @@ import MailsHead from "./mails/MailsHead";
 
 const MyMails = () => {
   return (
-    <div className="w-full pr-4">
+    <div className="w-full pr-4 h-full">
       <div className="h-full w-full bg-white rounded-t-2xl p-3">
-        <MailsHead/>
+        <MailsHead />
       </div>
     </div>
   );
