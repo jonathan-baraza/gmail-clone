bb6afb9b3bfb8c613af40ab36f268cfb3efb8c6d
jonathan_baraza <jbaraza46@gmail.com>
2023-09-10 11:50:42 +0300

    selects feat

--- components/mailPage/mails/MailsHead.tsx
+++ components/mailPage/mails/MailsHead.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { BsSquare } from "react-icons/bs";
+import { BiSquare } from "react-icons/bi";
+import { AiOutlineCaretDown } from "react-icons/ai";
 
 const MailsHead = () => {
   return (
-    <div>MailsHead</div>
-  )
-}
+    <div className="w-full">
+      <div className="flex items-center">
+        <div className="hover:bg-[#f2f2f2] py-3 w-fit rounded px-2 hover:cursor-pointer">
+          <BiSquare size={15} color={"#5f6368"} className="font-bold" />
+        </div>
+        <div className="hover:bg-[#f2f2f2] py-3 w-fit rounded px-1 hover:cursor-pointer">
+          <AiOutlineCaretDown
+            size={15}
+            color={"#5f6368"}
+            className="font-bold"
+          />
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default MailsHead
+export default MailsHead;
