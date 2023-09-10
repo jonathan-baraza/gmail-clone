2344ea3f94a41b85629b035dd666263946902c42
jonathan_baraza <jbaraza46@gmail.com>
2023-09-10 12:05:18 +0300

    num of emails

--- components/mailPage/mails/MailsHead.tsx
+++ components/mailPage/mails/MailsHead.tsx
@@ -6,7 +6,7 @@ import { MdMoreVert, MdRefresh } from "react-icons/md";
 
 const MailsHead = () => {
   return (
-    <div className="w-full">
+    <div className="w-full flex">
       <div className="flex items-center">
         <div className="hover:bg-[#f2f2f2] py-[10px] w-fit rounded px-1 hover:cursor-pointer">
           <BiSquare size={18} color={"#5f6368"} className="font-bold" />
@@ -18,14 +18,16 @@ const MailsHead = () => {
             className="font-bold"
           />
         </div>
-        <div className="hover:bg-[#e8eaed] rounded-full p-2 hover:cursor-pointer">
+        <div className="hover:bg-[#e8eaed] rounded-full p-2 ml-3 hover:cursor-pointer">
           <MdRefresh size={20} color={"#5f6368"} />
         </div>
         <div className="hover:bg-[#e8eaed] rounded-full p-2 hover:cursor-pointer">
           <MdMoreVert size={20} color={"#5f6368"} />
         </div>
       </div>
-      <div></div>
+      <div>
+        <span className="text-[#6b6b6b] text-xs">1-5 of 5,001</span>
+      </div>
     </div>
   );
 };
