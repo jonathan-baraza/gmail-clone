c2b2d0885c6249db97a03fa2a62f4b783a618c04
jonathan_baraza <jbaraza46@gmail.com>
2023-09-13 20:26:02 +0300

    toggle color view

--- components/mailPage/mails/MailsTabs.tsx
+++ components/mailPage/mails/MailsTabs.tsx
@@ -6,12 +6,15 @@ const MailsTabs = () => {
   return (
     <div className="w-full">
       <div
-        className="flex items-center w-1/4 hover:bg-[#e8eaed] hover:cursor-pointer"
-        onClick={() => setActiveTab("primarsy")}
+        className="flex items-center w-1/4 hover:bg-[#e8eaed] hover:cursor-pointer p-3"
+        onClick={() => setActiveTab("primary")}
       >
-        <HiInbox color={`${activeTab === "primary" ? "#0b57d0" : "#444746"}`} />
+        <HiInbox
+          size={20}
+          color={`${activeTab === "primary" ? "#0b57d0" : "#444746"}`}
+        />
         <span
-          className={`${
+          className={`ml-3 ${
             activeTab === "primary" ? "text-[#0b57d0]" : "text-[#444746]"
           }`}
         >
