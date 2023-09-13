ef5d8e5dfbf724d15c6e54308e77a9f4b0440872
jonathan_baraza <jbaraza46@gmail.com>
2023-09-13 20:25:28 +0300

    fix: left padding

--- components/mailPage/MyMails.tsx
+++ components/mailPage/MyMails.tsx
@@ -5,7 +5,7 @@ import MailsTabs from "./mails/MailsTabs";
 const MyMails = () => {
   return (
     <div className="w-full pr-4 h-full">
-      <div className="h-full w-full bg-white rounded-t-2xl pt-[6px] px-[10px]">
+      <div className="h-full w-full bg-white rounded-t-2xl pt-[6px]">
         <MailsHead />
         <MailsTabs/>
       </div>
