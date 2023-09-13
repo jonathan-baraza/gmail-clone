09e3899d7f0f713ebddfaa86b20716118493f0d9
jonathan_baraza <jbaraza46@gmail.com>
2023-09-13 20:25:43 +0300

    fix: left padding

--- components/mailPage/mails/MailsHead.tsx
+++ components/mailPage/mails/MailsHead.tsx
@@ -12,7 +12,7 @@ import { HiChevronLeft, HiChevronRight } from "react-icons/hi";
 
 const MailsHead = () => {
   return (
-    <div className="w-full flex items-center justify-between ">
+    <div className="w-full flex items-center justify-between px-[10px]">
       <div className="flex items-center">
         <div className="hover:bg-[#f2f2f2] py-[10px] w-fit rounded px-1 hover:cursor-pointer">
           <BiSquare size={18} color={"#5f6368"} className="font-bold" />
