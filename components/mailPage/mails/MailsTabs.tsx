f5b1d7dec4f36d1c5628d6e97b546f4556c08c63
jonathan_baraza <jbaraza46@gmail.com>
2023-09-13 20:19:46 +0300

    primary tab

--- components/mailPage/mails/MailsTabs.tsx
+++ components/mailPage/mails/MailsTabs.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import {useState} from "react";
+import { HiInbox } from "react-icons/hi";
 
 const MailsTabs = () => {
+    const [activeTab,setActiveTab]=useState<string>("primary")
   return (
-    <div>MailsTabs</div>
-  )
-}
+    <div className="w-full">
+      <div className="flex items-center w-1/4 hover:bg-[#e8eaed] hover:cursor-pointer">
+        <HiInbox color={`${activeTab === "primary" ? "#0b57d0" : "#444746"}`} />
+        <span>Primary</span>
+      </div>
+    </div>
+  );
+};
 
-export default MailsTabs
+export default MailsTabs;
