1b249e0b2bf85ea428bcd9bdc2fc1304800b9f26
jonathan_baraza <jbaraza46@gmail.com>
2023-09-13 20:08:53 +0300

    included tabs

--- components/mailPage/MyMails.tsx
+++ components/mailPage/MyMails.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MailsHead from "./mails/MailsHead";
+import MailsTabs from "./mails/MailsTabs";
 
 const MyMails = () => {
   return (
     <div className="w-full pr-4 h-full">
       <div className="h-full w-full bg-white rounded-t-2xl pt-[6px] px-[10px]">
         <MailsHead />
+        <MailsTabs/>
       </div>
     </div>
   );
