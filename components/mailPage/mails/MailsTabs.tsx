eebc7f66f5daadca68977e151c28c511973fc0da
jonathan_baraza <jbaraza46@gmail.com>
2023-09-13 20:35:10 +0300

    head icons revamp

--- components/mailPage/mails/MailsTabs.tsx
+++ components/mailPage/mails/MailsTabs.tsx
@@ -6,20 +6,28 @@ const MailsTabs = () => {
   return (
     <div className="w-full">
       <div
-        className="flex items-center w-1/4 hover:bg-[#e8eaed] hover:cursor-pointer p-3"
+        className="flex justify-center w-1/4 hover:bg-[#e8eaed] hover:cursor-pointer px-2 "
         onClick={() => setActiveTab("primary")}
       >
-        <HiInbox
-          size={20}
-          color={`${activeTab === "primary" ? "#0b57d0" : "#444746"}`}
-        />
-        <span
-          className={`ml-3 ${
-            activeTab === "primary" ? "text-[#0b57d0]" : "text-[#444746]"
+        <div
+          className={`flex items-center w-full py-4 px-2 ${
+            activeTab === "primary"
+              ? "border-b-[3px] border-[#0b57d0]"
+              : "border-none"
           }`}
         >
-          Primary
-        </span>
+          <HiInbox
+            size={20}
+            color={`${activeTab === "primary" ? "#0b57d0" : "#444746"}`}
+          />
+          <span
+            className={`ml-3 ${
+              activeTab === "primary" ? "text-[#0b57d0]" : "text-[#444746]"
+            }`}
+          >
+            Primary
+          </span>
+        </div>
       </div>
     </div>
   );
