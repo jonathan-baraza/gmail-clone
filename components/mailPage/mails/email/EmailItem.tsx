5d93edaf1b5f44493a14b1476c2f89f96d975d49
jonathan_baraza <jbaraza46@gmail.com>
2023-09-16 12:00:57 +0300

    email Item init

--- components/mailPage/mails/email/EmailItem.tsx
+++ components/mailPage/mails/email/EmailItem.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BiSquare, BiStar } from "react-icons/bi";
+import { MdLabelImportantOutline } from "react-icons/md";
 
 const EmailItem = () => {
   return (
-    <div>EmailItem</div>
-  )
-}
+    <div>
+      <div className="flex">
+        <div className="hover:bg-[#e8eaed] rounded-full p-2 ml-3 hover:cursor-pointer">
+          <BiSquare size={18} color={"#5f6368"} />
+        </div>
+        <div className="hover:bg-[#e8eaed] rounded-full p-2 ml-3 hover:cursor-pointer">
+          <BiStar size={20} color={"#5f6368"} />
+        </div>
+        <div className="hover:bg-[#e8eaed] rounded-full p-2 ml-3 hover:cursor-pointer">
+          <MdLabelImportantOutline size={20} color={"#5f6368"} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EmailItem
+export default EmailItem;
