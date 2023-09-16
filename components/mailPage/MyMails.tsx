f0115fedab3fd68beff6f0e6bbe75993c80fd97c
jonathan_baraza <jbaraza46@gmail.com>
2023-09-16 11:56:07 +0300

    emails container inclusion

--- components/mailPage/MyMails.tsx
+++ components/mailPage/MyMails.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import MailsHead from "./mails/MailsHead";
 import MailsTabs from "./mails/MailsTabs";
+import EmailsContainer from "./mails/email/EmailsContainer";
 
 const MyMails = () => {
   return (
     <div className="w-full pr-4 h-full">
-      <div className="h-full w-full bg-white rounded-t-2xl pt-[6px]">
+      <div className="h-full w-full bg-white rounded-t-2xl pt-[5px]">
         <MailsHead />
         <MailsTabs/>
+        <EmailsContainer/>
       </div>
     </div>
   );
