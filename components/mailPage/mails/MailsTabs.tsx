b3ebe4236700d5e963466eb9e04427286a2cd17c
jonathan_baraza <jbaraza46@gmail.com>
2023-09-16 11:44:07 +0300

    social tab done

--- components/mailPage/mails/MailsTabs.tsx
+++ components/mailPage/mails/MailsTabs.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { HiInbox, HiOutlineTag, HiTag } from "react-icons/hi";
+import { HiInbox, HiOutlineTag, HiOutlineUsers, HiTag } from "react-icons/hi";
 
 const MailsTabs = () => {
   const [activeTab, setActiveTab] = useState<string>("primary");
@@ -55,6 +55,31 @@ const MailsTabs = () => {
             }`}
           >
             Promotions
+          </span>
+        </div>
+      </div>
+
+      {/* Social */}
+      <div
+        className="flex justify-center w-1/4 hover:bg-[#f4f4f4] hover:cursor-pointer px-2 "
+        onClick={() => setActiveTab("social")}
+      >
+        <div
+          className={`flex border-b-[3px] items-center w-full py-[16px] px-2  ${
+            activeTab === "social" ? " border-[#0b57d0]" : "border-transparent"
+          }`}
+        >
+          <HiOutlineUsers
+            size={20}
+            color={`${activeTab === "social" ? "#0b57d0" : "#444746"}`}
+          />
+
+          <span
+            className={`ml-3 text-sm font-semibold ${
+              activeTab === "social" ? "text-[#0b57d0]" : "text-[#444746]"
+            }`}
+          >
+            Social
           </span>
         </div>
       </div>
