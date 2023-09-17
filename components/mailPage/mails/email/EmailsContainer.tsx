b7b39b8786f2014c59bb40ec2731f18395d38a40
jonathan_baraza <jbaraza46@gmail.com>
2023-09-17 15:46:01 +0300

    passed down email details as props

--- components/mailPage/mails/email/EmailsContainer.tsx
+++ components/mailPage/mails/email/EmailsContainer.tsx
@@ -4,7 +4,13 @@ import EmailItem from "./EmailItem";
 const EmailContainer = () => {
   return (
     <div>
-      <EmailItem />
+      <EmailItem
+        subject={"Meta Learning Program"}
+        body={"Final learning week!!!You have been added to week 7!!"}
+        time={"12:14"}
+        isImportant={false}
+        isStarred={false}
+      />
     </div>
   );
 };
